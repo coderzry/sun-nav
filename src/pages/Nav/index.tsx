03b55b51452147d6c97e1fd0d8e8f40cb8f40f7c
赵瑞阳 <zhaoruiyang@icourt.cc>
2023-09-04 15:57:40 +0800

    feat: add nav demo

--- src/pages/Nav/index.tsx
+++ src/pages/Nav/index.tsx
@@ -1,16 +1,24 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
-import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
+import { Tooltip } from 'antd';
 import styles from './index.less';
 
-const HomePage: React.FC = () => {
+const Nav: React.FC = () => {
   const { name } = useModel('global');
   return (
-    <div>
-      <h2>我是导航页</h2>
+    <div className={styles.container}>
+      <Tooltip placement="top" title={'遥遥领先遥遥领先遥遥领先遥遥领先遥遥领先'}>
+        <a className={styles.card} target='_blank' href='https://www.aigaixie.com/'>
+          <div className={styles.left}>
+            <img className={styles.img} src="https://ai-bot.cn/wp-content/uploads/2023/04/xiezuocat-ai-icon.png" alt="" />
+          </div>
+          <div className={styles.right}>
+            <div className={styles.title}>欢迎使用</div>
+            <div className={styles.desc}>欢迎使用欢迎使用欢迎使用</div>
+          </div>
+        </a>
+      </Tooltip>
     </div>
   );
 };
 
-export default HomePage;
+export default Nav;
